175f3deee6674141f940c50cd52eb3fc0cc1363d
Gerardo Alberto Ramirez Mejorada <147885471+Pusstes@users.noreply.github.com>
2025-01-30 23:05:50 -0600

    Termino Practica 2

---
+++ FASTAPI/app.py
@@ -0,0 +1,66 @@
+from fastapi import FastAPI
+from typing import Optional
+
+#declaramos un objeto 
+app = FastAPI(
+    title='Mi primer API 196', 
+    description='Gerardo Alberto ramirez',
+    version='1.0.1'
+)
+
+usuarios= [
+    {'id':1, 'nombre':'Gerardo', 'edad': 20},
+    {'id':2, 'nombre':'Alberto', 'edad': 20},
+    {'id':3, 'nombre':'Ramirez', 'edad': 20},
+    {'id':4, 'nombre':'Gerardo Alberto', 'edad': 23},
+    {'id':5, 'nombre':'America', 'edad': 24},
+    {'id':6, 'nombre':'Chivas', 'edad': 25}
+]
+        
+#generamos nuestro primer endpoint 
+# endpoint tipo get 
+@app.get('/', tags=['Inicio'])
+def main():
+    return {'hello FastAPI':'Gerardo Alberto'}
+
+@app.get('/promedio', tags=['Mi Calificacion TAI'])
+def promedio():
+    return 5*2
+
+# end point parametro obligatorio
+@app.get('/usuario/{id}', tags=['Parametro obligatorio'])
+def consultaUsuario(id: int):
+    return {'se encontro el usuario': id}
+
+# endPoint parametro opcional
+@app.get('/usuario2/', tags=['Parametro Opcional'])
+def consultarUsuarioOpcional(id: Optional[int] = None):
+    if id is not None:
+        for usuario in usuarios:
+            if usuario['id'] == id: 
+                return {'Usuario encontrado': usuario}
+        return {'Mensaje':f'No se encontrado el id: {id}'}
+    else:
+        return{'mensaje': 'No hay usuarios registrados'}
+    
+    #endpoint con varios parametro opcionales
+@app.get("/usuarios/", tags=["3 parámetros opcionales"])
+async def consulta_usuarios(
+    usuario_id: Optional[int] = None,
+    nombre: Optional[str] = None,
+    edad: Optional[int] = None
+):
+    resultados = []
+
+    for usuario in usuarios:
+        if (
+            (usuario is None or usuario["id"] == usuario_id) and
+            (nombre is None or usuario["nombre"].lower() == nombre.lower()) and
+            (edad is None or usuario["edad"] == edad)
+        ):
+            resultados.append(usuario)
+
+    if resultados:
+        return {"usuarios_encontrados": resultados}
+    else:
+        return {"mensaje": "No se encontraron usuarios que coincidan con los parámetros proporcionados."+str(usuario_id)+str(nombre)+str(edad)}
--- FASTAPI/main.py
+++ FASTAPI/main.py
@@ -1,7 +1,39 @@
-from fastapi import FastAPI
+from fastapi import FastAPI 
+# importamos la libreria para permitir datos opcionales
+from typing import Optional
 
-app = FastAPI()
+# declaracion del objeto y la instaciamos de la clase FastAPI
+app = FastAPI(
+    title='Mi primer API 196',
+    description='Estrella Cuellar',
+    version='1.0.1'
+)
 
-@app.get('/')
+#Creamos una lista para la base de datos 
+usuarios=[
+    {'id':1, 'nombre':'Estrella', 'edad':20},
+    {'id':2, 'nombre':'Lu', 'edad':20},
+    {'id':3, 'nombre':'Lalo', 'edad':21},
+    {'id':4, 'nombre':'Domi', 'edad':20},
+]
+
+
+# endpoint de arranque 
+@app.get('/', tags=['Inicio'])
+# metodo principal
 def main():
-    return {'hola FastAPI': 'Gerardo Alberto'}
+    # retornamos en formato JSON un mensaje 
+    return {'Hola FASTAPI':'EstrellaCuellar'}
+
+@app.get('/promedio', tags=['Mi claificaión'])
+def promedio():
+    return {'Promedio': 9.9}
+
+#endpoint obligatorio
+@app.get('/usuario/{id}', tags=['Parametro obligatorio'])
+def consultarusuario(id:int):
+    #conectamosBD
+    #hacemos consulta retornamos resultset 
+    return{'Se encontro el usuario': id}
+
+#endpoint opcional
