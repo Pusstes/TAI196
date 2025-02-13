ab9821f7cf590ec0348cb680304e3fe0ba018add
Gerardo Alberto Ramirez Mejorada <147885471+Pusstes@users.noreply.github.com>
2025-02-12 19:05:41 -0600

    Avanzamos en los end points de cruds

--- FASTAPI/app.py
+++ FASTAPI/app.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+
 from typing import Optional
 
 #declaramos un objeto 
@@ -17,50 +18,64 @@ usuarios= [
     {'id':6, 'nombre':'Chivas', 'edad': 25}
 ]
         
-#generamos nuestro primer endpoint 
-# endpoint tipo get 
-@app.get('/', tags=['Inicio'])
-def main():
-    return {'hello FastAPI':'Gerardo Alberto'}
+# #generamos nuestro primer endpoint 
+# # endpoint tipo get 
+# @app.get('/', tags=['Inicio'])
+# def main():
+#     return {'hello FastAPI':'Gerardo Alberto'}
 
-@app.get('/promedio', tags=['Mi Calificacion TAI'])
-def promedio():
-    return 5*2
+# @app.get('/promedio', tags=['Mi Calificacion TAI'])
+# def promedio():
+#     return 5*2
 
-# end point parametro obligatorio
-@app.get('/usuario/{id}', tags=['Parametro obligatorio'])
-def consultaUsuario(id: int):
-    return {'se encontro el usuario': id}
+# # end point parametro obligatorio
+# @app.get('/usuario/{id}', tags=['Parametro obligatorio'])
+# def consultaUsuario(id: int):
+#     return {'se encontro el usuario': id}
 
-# endPoint parametro opcional
-@app.get('/usuario2/', tags=['Parametro Opcional'])
-def consultarUsuarioOpcional(id: Optional[int] = None):
-    if id is not None:
-        for usuario in usuarios:
-            if usuario['id'] == id: 
-                return {'Usuario encontrado': usuario}
-        return {'Mensaje':f'No se encontrado el id: {id}'}
-    else:
-        return{'mensaje': 'No hay usuarios registrados'}
+# # endPoint parametro opcional
+# @app.get('/usuario2/', tags=['Parametro Opcional'])
+# def consultarUsuarioOpcional(id: Optional[int] = None):
+#     if id is not None:
+#         for usuario in usuarios:
+#             if usuario['id'] == id: 
+#                 return {'Usuario encontrado': usuario}
+#         return {'Mensaje':f'No se encontrado el id: {id}'}
+#     else:
+#         return{'mensaje': 'No hay usuarios registrados'}
     
-    #endpoint con varios parametro opcionales
-@app.get("/usuarios/", tags=["3 parámetros opcionales"])
-async def consulta_usuarios(
-    usuario_id: Optional[int] = None,
-    nombre: Optional[str] = None,
-    edad: Optional[int] = None
-):
-    resultados = []
+#     #endpoint con varios parametro opcionales
+# @app.get("/usuarios/", tags=["3 parámetros opcionales"])
+# async def consulta_usuarios(
+#     usuario_id: Optional[int] = None,
+#     nombre: Optional[str] = None,
+#     edad: Optional[int] = None
+# ):
+#     resultados = []
 
-    for usuario in usuarios:
-        if (
-            (usuario is None or usuario["id"] == usuario_id) and
-            (nombre is None or usuario["nombre"].lower() == nombre.lower()) and
-            (edad is None or usuario["edad"] == edad)
-        ):
-            resultados.append(usuario)
+#     for usuario in usuarios:
+#         if (
+#             (usuario is None or usuario["id"] == usuario_id) and
+#             (nombre is None or usuario["nombre"].lower() == nombre.lower()) and
+#             (edad is None or usuario["edad"] == edad)
+#         ):
+#             resultados.append(usuario)
 
-    if resultados:
-        return {"usuarios_encontrados": resultados}
-    else:
-        return {"mensaje": "No se encontraron usuarios que coincidan con los parámetros proporcionados."+str(usuario_id)+str(nombre)+str(edad)}
+#     if resultados:
+#         return {"usuarios_encontrados": resultados}
+#     else:
+#         return {"mensaje": "No se encontraron usuarios que coincidan con los parámetros proporcionados."+str(usuario_id)+str(nombre)+str(edad)}
+
+# end poin consultar todos los usuarios
+@app.get('/usuarios/', tags=['Operaciones CRUD'])
+def ConsultarTodos():
+    return {'Usuarios Registrados':usuarios}
+
+# end point para guardar un usuario tipo post
+@app.post('/usuario/', tags=['Operaciones CRUD'])
+def guardarUsuario(usuarionuevo: dict):
+    for usr in usuarios:
+        if usr['id'] == usuarionuevo.get('id'):
+            raise HTTPException(status_code=400, detail='El usuario ya esta registrado')
+    usuarios.append(usuarionuevo)
+    return {'mensaje':'Usuario registrado'}
