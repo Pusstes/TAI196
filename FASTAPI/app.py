b7f24a530fce55de7c0e3c72d69c3f78490d665f
Gerardo Alberto Ramirez Mejorada <147885471+Pusstes@users.noreply.github.com>
2025-03-27 18:46:15 -0600

    Practica 9 delete y actualizar

--- FASTAPI/app.py
+++ FASTAPI/app.py
@@ -96,7 +96,7 @@ def ConsultarTodos():
     return usuarios
 
 # end point para guardar un usuario tipo post
-@app.post('/usuario/',response_model= modelUsuario ,tags=['Operaciones CRUD']) #se agrega el modelo de respuesta
+@app.post('/usuario/',response_model= modelUsuario ,tags=['OPERACIONES CRUD']) #se agrega el modelo de respuesta
 def guardarUsuario(usuarionuevo: modelUsuario): #se agrega el modelo de entrada
     db = Session() #se crea la sesion
     try: #se intenta guardar el usuario
@@ -144,8 +144,8 @@ def ConsultarTodos():
     finally:
         db.close()
         
-@app.get('/usuario/{id}', tags=['Parametro obligatorio'])
-def consultaUsuario(id: int):
+@app.get('/usuario/{id}', tags=['OPERACIONES CRUD'])
+def consultaUsuario_por_id(id: int):
     db = Session()
     try:
         consulta = db.query(User).filter(User.id == id).first()
@@ -154,6 +154,45 @@ def consultaUsuario(id: int):
         return JSONResponse(content=jsonable_encoder(consulta))
     
     except Exception as x:
+        return JSONResponse(status_code=500, content={'mensaje':'No fue posible hacer la consulta', 'error':str(x)})
+    
+    finally:
+        db.close()
+   
+#end point para eliminar un usuario tipo delete     
+@app.delete('/usuario/{id}', tags=['OPERACIONES CRUD'])
+def borrarUsuario(id: int): #se recibe el id del usuario a eliminar se pone que es de tipo int
+    db = Session() #se crea la sesion de la base de datos
+    try: #se intenta hacer la consulta
+        consulta = db.query(User).filter(User.id == id).first() #se hace la consulta 
+        if not consulta: #se valida si el usuario existe
+            return JSONResponse(status_code=404, content={'mensaje':'Usuario no encontrado'}) #se retorna un mensaje de error
+        db.delete(consulta) #se elimina el usuario
+        db.commit() #se guarda la transaccion
+        return JSONResponse(content={'mensaje':'Usuario eliminado'}) #se retorna un mensaje de exito
+    
+    except Exception as x: #en caso de error se retorna un mensaje de error
+        db.rollback() #se deshace la transaccion
+        return JSONResponse(status_code=500, content={'mensaje':'No fue posible hacer la consulta', 'error':str(x)}) #se retorna un mensaje de error
+    
+    finally:
+        db.close() #se cierra la conexion con la base de datos
+        
+#end point para actualizar un usuario tipo put con su id
+@app.put('/usuario/{id}', tags=['OPERACIONES CRUD'])
+def actualizarUsuario(id: int, usuarioActualizado: modelUsuario): #se recibe el id del usuario a actualizar y el modelo de usuario actualizado
+    db = Session() #se crea la sesion de la base de datos
+    try: #se intenta hacer la consulta
+        consulta = db.query(User).filter(User.id == id).first() #se hace la consulta
+        if not consulta: #se valida si el usuario existe
+            return JSONResponse(status_code=404, content={'mensaje':'Usuario no encontrado'}) #se retorna un mensaje de error
+        for key, value in usuarioActualizado.model_dump().items(): #.items() regresa una lista de tuplas con clave y valor y .model_dump() regresa el modelo de usuario actualizado
+            setattr(consulta, key, value) #setattr() asigna el valor a la clave en la consulta
+        db.commit()
+        return JSONResponse(content={'mensaje':'Usuario actualizado'})
+    
+    except Exception as x:
+        db.rollback()
         return JSONResponse(status_code=500, content={'mensaje':'No fue posible hacer la consulta', 'error':str(x)})
     
     finally:
